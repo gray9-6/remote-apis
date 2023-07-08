923d18df051da4f17950e5813578506ca9014cf6
Ajay Kumar <aky22896@gmail.com>
2023-07-08 17:51:57 +0530

    Add project

---
+++ src/main/java/com/example/remoteapis/RemoteApiController.java
@@ -0,0 +1,9 @@
+package com.example.remoteapis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remote")
+public class RemoteApiController {
+}
